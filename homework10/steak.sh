82daa37372eaf28f08ecff44b14bce6b357dc170
kazakbaevelaman <kazakbaevelaman066@gmail.com>
2024-05-22 04:51:47 +0000

    added \U0001F969 steak.sh

---
+++ homework10/steak.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -p "Please enter meat temperature: " temp
+if [[ $temp -ge 120 && $temp -le 130 ]]; then 
+	echo "rare"
+elif [[ $temp -ge 131 && $temp -le 140 ]]; then 
+	echo "medium rare"
+elif [[ $temp -ge 141 && $temp -le 150 ]]; then 
+	echo "medium"
+elif [[ $temp -ge 151 && $temp -le 160 ]]; then 
+	echo "medium well"
+elif [[ $temp -ge 161 && $temp -le 170 ]]; then 
+	echo "well done"
+else
+  echo "invalid temp"
+fi
